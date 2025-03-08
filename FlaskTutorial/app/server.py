1e936565eba94446cded71c49be8a3a8dcb97488
alireza-astane <apoorhass@gmail.com>
2025-03-08 05:42:38 +0330

    sessions

--- FlaskTutorial/app/server.py
+++ FlaskTutorial/app/server.py
@@ -1,18 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
+from flask import Flask, session, redirect, url_for, request
 
 app = Flask(__name__)
+app.secret_key = "any random string"
 
 
 @app.route("/")
-def student():
-    return render_template("student.html")
+def index():
+    if "username" in session:
+        username = session["username"]
+        return (
+            "Logged in as "
+            + username
+            + "<br>"
+            + "<b><a href = '/logout'>click here to log out</a></b>"
+        )
+    return "You are not logged in <br><a href = '/login'>" + "click here to log in</a>"
 
 
-@app.route("/result", methods=["POST", "GET"])
-def result():
+@app.route("/login", methods=["GET", "POST"])
+def login():
     if request.method == "POST":
-        result = request.form
-        return render_template("result.html", result=result)
+        session["username"] = request.form["username"]
+        return redirect(url_for("index"))
+    return """
+	
+   <form action = "" method = "post">
+      <p><input type = text name = username/></p>
+      <p<<input type = submit value = Login/></p>
+   </form>	
+    """
+
+
+@app.route("/logout")
+def logout():
+    # remove the username from the session if it is there
+    session.pop("username", None)
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
